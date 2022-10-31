1bd2f900e0c38d76fef8d21f289a79b8a13c4cbe
tr0n_xD <tr0n_xD@protonmail.com>
2022-10-31 20:47:44 +0100

    remove react-dom old code

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
     <React.StrictMode>
         <BrowserRouter>
             <Routes>
@@ -12,6 +14,5 @@ ReactDOM.render(
                 <Route path="/create" element={<App view='create'/>}/>
             </Routes>
         </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
