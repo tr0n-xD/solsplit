c6b723226e8ef6490c86609cb35ae74111656317
tr0n_xD <tr0n_xD@protonmail.com>
2022-11-01 01:06:07 +0100

    ui spacing

--- src/views/SolsplitMain.tsx
+++ src/views/SolsplitMain.tsx
@@ -6,7 +6,7 @@ export default function SolsplitMain() {
     return (
         <div className='flexColumn justifyStart' style={{minHeight: '300px', width: '450px'}}>
             <div className='flexRow gap5' style={{marginTop: '20px'}}>
-                <div>Welcome to Solsplit</div><img alt='' height='24px' src='heart-32.png' style={{marginTop: '2px'}}/>
+                <div>Welcome to Solsplit</div><img alt='' height='24px' width='24px' src='heart-32.png' style={{marginTop: '2px'}}/>
             </div>
             <div>Set up royalty sharing for your team.</div>
             <div>It's safe, secure and easy!</div>
