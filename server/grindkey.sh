fb898a4a9f42cbd265661c9a6682e0a009d3dca6
tr0n_xD <tr0n_xD@protonmail.com>
2022-11-02 19:49:30 +0100

    revert this file

--- server/grindkey.sh
+++ server/grindkey.sh
@@ -4,10 +4,4 @@
 # solana command line tools must be installed :)
 # remember to set executable flag: chmod +x grindkey.sh
 
-echo "Making keypairs in folder wallets/free..."
-mkdir -p wallets/free
-mkdir -p wallets/active
-cd wallets/free
-solana-keygen grind --starts-with SPL:5
-cd ../..
-echo "Done."
+solana-keygen grind --starts-with SPL:1
